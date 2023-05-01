1de85502cbe58eee4c3f2a731c2a7cfcc3febf95
tbemre <124307360+tbemre@users.noreply.github.com>
2023-05-01 14:06:14 +0000

    main

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include "tic_tac_toe.h"
-
+#define SIZE 3
 int main(){
-    int board[3][3];
+    int board[SIZE][SIZE];
     char shape_of_board[] = "   |   |   \n-----------\n   |   |   \n-----------\n   |   |   \n";
-    printf("%s",shape_of_board);
+    
     return 0;
 }
