b56abf1e3eb954521e182882c8e4bf49e7636531
tbemre <124307360+tbemre@users.noreply.github.com>
2023-05-01 14:02:11 +0000

    main

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include "tic_tac_toe.h"
 
 int main(){
-    
+    int board[3][3];
+    char shape_of_board[] = "   |   |   \n-----------\n   |   |   \n-----------\n   |   |   \n";
+    printf("%s",shape_of_board);
     return 0;
 }
